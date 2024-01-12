ed44692cf12c12fb28d4569cfa435e7f6a0c412f
Maxime Delporte <62073284+MaximeDelporte@users.noreply.github.com>
2024-01-12 16:40:13 +0100

    Project initialization

---
+++ rc-premier-league/rc_premier_leagueApp.swift
@@ -0,0 +1,17 @@
+//
+//  rc_premier_leagueApp.swift
+//  rc-premier-league
+//
+//  Created by afx on 12/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct rc_premier_leagueApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
