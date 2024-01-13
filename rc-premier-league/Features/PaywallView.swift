30ce33680ade5125b854f01c3d4ec0995dc7151c
Maxime Delporte <62073284+MaximeDelporte@users.noreply.github.com>
2024-01-14 00:59:57 +0100

    Add blur view when user isPurchasing.

--- rc-premier-league/Features/PaywallView.swift
+++ rc-premier-league/Features/PaywallView.swift
@@ -12,60 +12,80 @@ import SwiftUI
 struct PaywallView: View {
     
     @Binding var isPaywallPresented: Bool
+    @State var isPurchasing = false
     @State var currentOffering: Offering?
     
     var body: some View {
-        VStack(alignment: .leading, spacing: 16) {
-            Text("Premier League Pro")
-                .font(.largeTitle)
-                .fontWeight(.bold)
-                .frame(maxWidth: .infinity, alignment: .leading)
-            
-            Text("Experience football like never before! Unlock the full power of the app for an all-access pass to your favorite teams and players.")
-                .frame(maxWidth: .infinity, alignment: .leading)
-            
-            PaywallItemView(
-                imageName: "ranking",
-                description: "Access to all results. Live ranking."
-            )
-            .padding(.top, 24)
-            
-            PaywallItemView(
-                imageName: "notification",
-                description: "Be notified of all the important events concerning your favourite teams."
-            )
-            .padding(.vertical, 16)
-            
-            PaywallItemView(
-                imageName: "support",
-                description: "24/7 premium support."
-            )
-            .padding(.bottom, 24)
-            
-            if let offering = currentOffering {
-                ForEach(offering.availablePackages) { package in
-                    if let subscriptionPeriod = package.storeProduct.subscriptionPeriod {
-                        
-                        let title = "\(subscriptionPeriod.periodTitle), \(package.localizedPriceString)"
-                        
-                        Button(action: {}, label: {
-                            Text(title)
-                                .padding(.vertical, 16)
-                                .frame(maxWidth: .infinity, alignment: .center)
-                                .background(.blue)
-                                .foregroundStyle(.white)
-                                .cornerRadius(8)
-                        })
+        ZStack {
+            VStack(alignment: .leading, spacing: 16) {
+                Text("Premier League Pro")
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                
+                Text("Experience football like never before! Unlock the full power of the app for an all-access pass to your favorite teams and players.")
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                
+                PaywallItemView(
+                    imageName: "ranking",
+                    description: "Access to all results. Live ranking."
+                )
+                .padding(.top, 24)
+                
+                PaywallItemView(
+                    imageName: "notification",
+                    description: "Be notified of all the important events concerning your favourite teams."
+                )
+                .padding(.vertical, 16)
+                
+                PaywallItemView(
+                    imageName: "support",
+                    description: "24/7 premium support."
+                )
+                .padding(.bottom, 24)
+                
+                if let offering = currentOffering {
+                    ForEach(offering.availablePackages) { package in
+                        if let subscriptionPeriod = package.storeProduct.subscriptionPeriod {
+                            
+                            let title = "\(subscriptionPeriod.periodTitle), \(package.localizedPriceString)"
+                            
+                            Button(
+                                action: {
+                                    isPurchasing = true
+                                    
+                                    Purchases.shared.purchase(package: package) { (transaction, customerInfo, error, userCancelled) in
+                                        isPurchasing = false
+                                        if let customerInfo, customerInfo.entitlements["pro"]?.isActive == true {
+                                            // Unlock that great "pro" content
+                                        }
+                                    }
+                                },
+                                label: {
+                                    Text(title)
+                                        .padding(.vertical, 16)
+                                        .frame(maxWidth: .infinity, alignment: .center)
+                                        .background(.blue)
+                                        .foregroundStyle(.white)
+                                        .cornerRadius(8)
+                                }
+                            )
+                        }
                     }
                 }
             }
-        }
-        .padding(.horizontal, 36)
-        .onAppear {
-            Purchases.shared.getOfferings(completion: { (offerings, error) in
-                guard let offering = offerings?.current else { return }
-                self.currentOffering = offering
-            })
+            .padding(.horizontal, 36)
+            .onAppear {
+                Purchases.shared.getOfferings(completion: { (offerings, error) in
+                    guard let offering = offerings?.current else { return }
+                    self.currentOffering = offering
+                })
+            }
+            
+            Rectangle()
+                .background(.black)
+                .opacity(isPurchasing ? 0.28 : 0.0)
+                .ignoresSafeArea(.all)
         }
     }
 }
